f4cf64d29b78e484d95ac6751b0e4fc74b8f0a44
ibragimimrohim360-oss <ibragimimrohim360@gmail.com>
2026-08-01 21:57:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,14 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Token-ро аз Render мегирем, дар ин ҷо намевисем
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.effective_user.first_name
     await update.message.reply_text(f'Салом {user_name}! 👋\nБоти ман дар Render кор мекунад ✅\n\nФармонҳо:\n/start - Оғоз')
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Ин боти аввалини ман аст 😄\nТанҳо /start кор мекунад ҳозир')
-
 if __name__ == '__main__':
-    print("✅ Bot started...")
+    print("✅ Bot started on Render...")
     app = ApplicationBuilder().token(TOKEN).build()
-    
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
-    
     app.run_polling()
