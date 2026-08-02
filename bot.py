e42c55eac4dc950cca9e246981a7900aacccfb85
ibragimimrohim360-oss <ibragimimrohim360@gmail.com>
2026-08-02 13:51:06 +0500

    Refactor bot to use async and new Application class

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("8870950829:AAHrviexyWuaO1HFMH-zorVmecEAsWvfO8o")
 
-def start(update, context):
-    update.message.reply_text('Салом! Бот дар Render кор мекунад ✅')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Салом! Бот дар Render кор мекунад ✅')
 
 def main():
-    print("✅ Bot started on Render...")
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+    print("✅ Bot started...")
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
