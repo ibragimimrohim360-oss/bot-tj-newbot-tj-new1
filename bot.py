7cfed48c3727a74d6d1a7423450c2cd70b0802d4
ibragimimrohim360-oss <ibragimimrohim360@gmail.com>
2026-08-02 22:29:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import os
-from telegram.ext import Application, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")  # <-- аз Render мегирад, на аз ин ҷо
+TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Салом! Ман кор мекунам ✅')
 
 def main():
